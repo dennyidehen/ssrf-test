9ae5c17418a6a5dd11fb3eec943573f1c081eaf8
denny.idehen <denny.idehen@integromat.com>
2022-11-04 18:47:16 +0100

    test

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const hostname = "127.0.0.1"
+//const hostname = "127.0.0.1"
 const port = 3000;
 
 const server = http.createServer((req, res) => {
@@ -9,6 +9,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello universe');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running on ${hostname} ${port}/`);
+server.listen(port, () => {
+  console.log(`Server running on ${port}/`);
 });
