7551e2cf1b17fc80fa548577f7d822b0ddd81199
denny.idehen <denny.idehen@integromat.com>
2022-11-05 12:14:58 +0100

    test

--- test.js
+++ test.js
@@ -1,9 +1,14 @@
 const http = require('http');
 
 const port = 80;
+let content = "";
 
 const server = http.createServer((req, res) => {
-  const content = '{"list": [{"module": "test", "primary_model":"model_test", "models": ["model_test"]}]}'
+  if(req.url === "/api.php/me"){
+    content = "test";
+  }else if(req.url === "/api.php/meta/modules"){
+    content = '{"list": [{"module": "test", "primary_model":"model_test", "models": ["model_test"]}]}';
+  }
   res.statusCode = 200;
   //res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Content-Length', `${content.length}`);
